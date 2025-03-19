d144de30ef0b3d010eb016f0a8a7758181fba516
Aurélien St <aurelien.strimelle@bstorm.be>
2025-03-19 16:00:05 +0100

    Header 🌺

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Header from './containers/header/header.jsx';
 
 function App() {
 
   return (
     <>
+      <Header />
+      <main>
       
+      </main>
     </>
   )
 }
---
+++ src/containers/header/header.jsx
@@ -0,0 +1,9 @@
+import style from './header.module.css';
+
+const Header = () => (
+    <header className={style['header']}>
+        <span>Exo 07 - Requête</span>
+    </header>
+);
+
+export default Header;
